31307caa9e45a0d200abace7f1943faa11148365
jakemuntarsi <jake.munta@ruralsourcing.com>
2021-01-06 09:51:48 -0500

    Fixed minor review comments

--- test/algorithmiaTest.js
+++ test/algorithmiaTest.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const assert = this.chai ? this.chai.assert : require('assert');
-
+const assert = window.chai ? window.chai.assert : require('assert');
 describe('Algorithmia', function() {
     describe('client', function() {
-      
-        it('should invoke algo endpoint', function() {
-          assert.equal(document.getElementById("result").innerText, "Hello HAL 9000")
+        before(async () => {
+            var api_key = window.location.search.slice(1).split('=')[1];
+            await  Algorithmia.query("demo/Hello", api_key , "HAL 9000", (error, result)=>{
+                document.getElementById('result').innerText = result;
+            });
         });
-
+        it('should invoke algo endpoint',  async ()=> {
+           await  assert.equal(document.getElementById("result").innerText, "Hello HAL 9000");
+        });
     });
-
 });
