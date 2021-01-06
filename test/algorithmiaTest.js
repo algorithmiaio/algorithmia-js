72aacdd1c7a6bacc58b7104d2a0d1a6c3e272c96
jakemuntarsi <jake.munta@ruralsourcing.com>
2021-01-06 13:15:26 -0500

    Fixed review suggestions

--- test/algorithmiaTest.js
+++ test/algorithmiaTest.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const assert = window.chai ? window.chai.assert : require('assert');
-describe('Algorithmia', function() {
-    describe('client', function() {
-        before(async () => {
-            var api_key = window.location.search.slice(1).split('=')[1];
-            await  Algorithmia.query("demo/Hello", api_key , "HAL 9000", (error, result)=>{
-                document.getElementById('result').innerText = result;
-            });
-        });
-        it('should invoke algo endpoint',  async ()=> {
-           await  assert.equal(document.getElementById("result").innerText, "Hello HAL 9000");
-        });
+
+describe('Algorithmia JS client', () => {
+    it('should invoke algo endpoint', (done) => {
+        const apiKey = window.location.search.slice(1).split('=')[1];
+
+        Algorithmia.query(
+            'demo/Hello',
+            apiKey,
+            'HAL 9000',
+            (error, result) => {
+                assert.equal(result, 'Hello HAL 9000');
+                done();
+            }
+        );
     });
 });
